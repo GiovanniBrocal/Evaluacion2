102716c9f634d2c9ccd40dbedef41ee77e9aec1a
Giovanni <giovannibrocal54321@gmail.com>
2023-10-21 17:51:12 -0300

    primer commit

--- app/src/main/java/cl/gbrocal/evaluacion2/mapa.java
+++ app/src/main/java/cl/gbrocal/evaluacion2/mapa.java
@@ -1,14 +1,56 @@
 package cl.gbrocal.evaluacion2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class mapa extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapa extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        String dir1 = getIntent().getStringExtra("dir1");
+        String[] parts1 = dir1.split(",");
+        double la1 = Integer.parseInt(parts1[0]);
+        double lo1 = Integer.parseInt(parts1[1]);
+
+        String dir2 = getIntent().getStringExtra("dir2");
+        String[] parts2 = dir2.split(",");
+        double la2 = Integer.parseInt(parts2[0]);
+        double lo2 = Integer.parseInt(parts2[1]);
+
+        String dir3 = getIntent().getStringExtra("dir3");
+        String[] parts3 = dir3.split(",");
+        double la3 = Integer.parseInt(parts3[0]);
+        double lo3 = Integer.parseInt(parts3[1]);
+
+        LatLng Direct1 = new LatLng(la1, lo1);
+        LatLng Direct2 = new LatLng(la2, lo2);
+        LatLng Direct3 = new LatLng(la3, lo3);
+
+        mMap.addMarker(new MarkerOptions().position(Direct1).title("Direccion 1"));
+        mMap.addMarker(new MarkerOptions().position(Direct2).title("Direccion 2"));
+        mMap.addMarker(new MarkerOptions().position(Direct3).title("Direccion 3"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Direct1));
     }
 }
--- app/src/main/java/cl/gbrocal/evaluacion2/splash.java
+++ app/src/main/java/cl/gbrocal/evaluacion2/splash.java
@@ -2,13 +2,30 @@ package cl.gbrocal.evaluacion2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        MainActivity.class
+                );
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
